2cea0be83de437b70c140ff15d2b221f4d1db46a
es3a10 <es3a10@gmail.com>
2015-09-18 09:26:39 +0200

    15_16

---
+++ 15_16/acta_claustro_plantilla.rst
@@ -0,0 +1,25 @@
+**Asistentes**
+
+- Ausencias justificadas
+
+**Orden del día**
+
+En Cortes, siendo las 9:00 horas del día XX de XX de XX, se reúne el Claustro de Profesores del IESO “Bardenas Reales” para celebrar sesión de Claustro, según convocatoria hecha por el Director.
+
+Se procede con el orden del día:
+
+#. Aprobación1, si procede, de las actas correspondientes a las sesiones del XX y del XX que se pueden leer e imprimir desde la web.
+
+    Se procede a la aprobación y firma de las citadas actas.
+    
+#. Informaciones de interés
+
+    Santiago Paricio informa de los siguientes puntos:
+        - xxx
+        - 
+
+#. Aportaciones de los asistentes
+
+
+
+Y sin más asuntos que tratar se levanta la sesión a las XX horas.
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,15 +2,27 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+
 AUTHOR = u'Carlos Rodr\xedguez'
-SITENAME = u'Bit\xe1cora Personal'
+SITENAME = u'Bit\xe1cora Personaaaaaaaal'
 SITEURL = ''
 
 PATH = 'content'
+#THEME = "notmyidea"
+THEME = "simple"
 
 TIMEZONE = 'Europe/Paris'
-
 DEFAULT_LANG = u'es'
+STATIC_PATHS = ["images" ]
+LOAD_CONTENT_CACHE = False
+# The minimum number of articles allowed on the last page. Use this when you
+# don’t want to have a last page with very few articles.
+DEFAULT_ORPHANS = 0
+# The maximum number of articles to include on a page, not including orphans.
+DEFAULT_PAGINATION = 3
+#Activate pagination.
+WITH_PAGINATION = True
+
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
